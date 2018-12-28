78eca9340f127b1a58dac7c14b2fd91b158c5609
Abigail Rettew <abigailrettew@Abigails-MBP.lan1>
2018-12-27 23:06:14 -0500

    Added templates

--- games/views.py
+++ games/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 from django.views.generic import TemplateView
 
 class HomeView(TemplateView):
-    template_name = "base.html"
+    template_name = "homepage.html"
 
 class ArraysView(TemplateView): 
     template_name = "arrays.html"
